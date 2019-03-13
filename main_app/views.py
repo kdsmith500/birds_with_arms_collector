60557fc1a1b5118eebb0401e240017340ee989e6
Kyle Smith <k.d.smith@gmail.com>
2019-03-13 10:51:10 -0500

    first commit! app with about, index, and details

---
+++ main_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Bird
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def birds_index(request):
+    birds = Bird.objects.all()
+    return render(request, 'birds/index.html', { 'birds': birds })
+
+def birds_detail(request, bird_id):
+  bird = Bird.objects.get(id=bird_id)
+  return render(request, 'birds/detail.html', { 'bird': bird })
